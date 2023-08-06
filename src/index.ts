e5541c722ca9e2adbd2bbabb2b2472245dc6ea77
SSOfy <support@ssofy.com>
2023-08-06 16:39:07 +0300

    - wrapped models under Models namespace. - wrapped entity models under Entities namespace. - wrapped repository interfaces under Repositories namespace.

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,44 @@
+import * as APIResponseTypes from "./Models/APIResponse";
+import * as SignatureTypes from "./Models/Signature";
+import * as TokenTypes from "./Models/Token";
+
+import * as AuthResponse from "./Models/Entities/AuthResponseEntity";
+import * as Token from "./Models/Entities/TokenEntity";
+import * as Client from "./Models/Entities/ClientEntity";
+import * as OTPOption from "./Models/Entities/OTPOptionEntity";
+import * as Scope from "./Models/Entities/ScopeEntity";
+import * as User from "./Models/Entities/UserEntity";
+
+import * as ClientRepo from "./Repositories/ClientRepository";
+import * as OTPRepo from "./Repositories/OTPRepository";
+import * as ScopeRepo from "./Repositories/ScopeRepository";
+import * as UserRepo from "./Repositories/UserRepository";
+
 export {APIConfig} from "./APIConfig";
 export {APIClient} from "./APIClient";
 export {OAuth2Config} from "./OAuth2Config";
 export {OAuth2Client} from "./OAuth2Client";
 export {SignatureGenerator} from "./SignatureGenerator";
 export {SignatureVerifier} from "./SignatureVerifier";
-export {APIResponse} from "./Models/APIResponse";
-export {Signature} from "./Models/Signature";
-export {Token} from "./Models/Token";
-export {AuthResponseEntity} from "./Models/Entities/AuthResponseEntity";
-export {TokenEntity} from "./Models/Entities/TokenEntity";
-export {ClientEntity} from "./Models/Entities/ClientEntity";
-export {OTPOptionEntity} from "./Models/Entities/OTPOptionEntity";
-export {ScopeEntity} from "./Models/Entities/ScopeEntity";
-export {UserEntity} from "./Models/Entities/UserEntity";
-export {ClientRepository} from "./Repositories/ClientRepository";
-export {OTPRepository} from "./Repositories/OTPRepository";
-export {ScopeRepository} from "./Repositories/ScopeRepository";
-export {UserRepository} from "./Repositories/UserRepository";
+
+export namespace Models {
+    export type APIResponse = APIResponseTypes.APIResponse;
+    export type Signature = SignatureTypes.Signature;
+    export type Token = TokenTypes.Token;
+}
+
+export namespace Entities {
+    export type AuthResponseEntity = AuthResponse.AuthResponseEntity;
+    export type TokenEntity = Token.TokenEntity;
+    export type ClientEntity = Client.ClientEntity;
+    export type OTPOptionEntity = OTPOption.OTPOptionEntity;
+    export type ScopeEntity = Scope.ScopeEntity;
+    export type UserEntity = User.UserEntity;
+}
+
+export namespace Repositories {
+    export type ClientRepository = ClientRepo.ClientRepository;
+    export type OTPRepository = OTPRepo.OTPRepository;
+    export type ScopeRepository = ScopeRepo.ScopeRepository;
+    export type UserRepository = UserRepo.UserRepository;
+}
