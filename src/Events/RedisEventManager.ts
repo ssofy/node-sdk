b02aba0401b8ba513c521b277642e3f9e175c1d8
SSOfy <support@ssofy.com>
2023-08-16 20:01:27 +0300

    added redis event manager

---
+++ src/Events/RedisEventManager.ts
@@ -0,0 +1,75 @@
+import {EventManager} from "./EventManager";
+
+export class RedisEventManager extends EventManager {
+    private publisher: any;
+    private subscriber: any;
+    private readonly channelPrefix: string;
+
+    private messageQueue: Array<{ event: string, message: any }> = [];
+    private handlers: { [event: string]: Array<(event: string, message?: any) => void> } = {};
+
+    constructor(publisher: any, subscriber: any, channelPrefix: string = 'sso:') {
+        super();
+        this.publisher = publisher;
+        this.subscriber = subscriber;
+        this.channelPrefix = channelPrefix;
+
+        this.subscriber.on('ready', this.resubscribe.bind(this));
+        this.publisher.on('ready', this.flushQueue.bind(this));
+    }
+
+    emit(event: string, message?: any): void {
+        if (this.publisher.isReady) {
+            this.actualEmit(event, message);
+            return;
+        }
+
+        this.messageQueue.push({event, message});
+    }
+
+    listen(event: string, handler: (event: string, message?: any) => void): void {
+        if (!this.handlers[event]) {
+            this.handlers[event] = [];
+        }
+
+        this.handlers[event].push(handler);
+
+        if (this.subscriber.isReady) {
+            this.actualSubscribe(event);
+        }
+    }
+
+    private actualEmit(event: string, message: any): void {
+        this.publisher.publish(this.channelPrefix + event, JSON.stringify(message ?? {}));
+    }
+
+    private actualSubscribe(event: string): void {
+        this.subscriber.subscribe(this.channelPrefix + event, (message: any) => {
+            console.log(this.handlers);
+            if (!this.handlers[event]) {
+                return;
+            }
+
+            const parsedMessage = JSON.parse(message ?? '{}');
+            for (const handler of this.handlers[event]) {
+                handler(event, parsedMessage);
+            }
+        });
+    }
+
+    private flushQueue(): void {
+        setTimeout(() => {
+            while (this.messageQueue.length > 0) {
+                const {event, message} = this.messageQueue.shift()!;
+                this.actualEmit(event, message);
+            }
+        }, 5000);
+    }
+
+    private resubscribe(): void {
+        const events = Object.keys(this.handlers);
+        for (const event of events) {
+            this.actualSubscribe(event);
+        }
+    }
+}
--- src/Events/index.ts
+++ src/Events/index.ts
@@ -1,7 +1,9 @@
 import * as EventManagerInternal from "./EventManager";
 import * as DefaultEventManagerInternal from "./DefaultEventManager";
+import * as RedisEventManagerInternal from "./RedisEventManager";
 
 export namespace Events {
     export import EventManager = EventManagerInternal.EventManager;
     export import DefaultEventManager = DefaultEventManagerInternal.DefaultEventManager;
+    export import RedisEventManager = RedisEventManagerInternal.RedisEventManager;
 }
