c3e434cc3533604debd372579808be793ce0bff5
SSOfy <support@ssofy.com>
2023-08-26 13:43:21 +0300

    wrapped storage classes from javascript-sdk

---
+++ src/Storages/index.ts
@@ -0,0 +1,8 @@
+import * as JavascriptSDK from "@ssofy/javascript-sdk";
+
+export namespace Storages {
+    export import Storage = JavascriptSDK.Storage;
+    export import NullStorage = JavascriptSDK.NullStorage;
+    export import MemoryStorage = JavascriptSDK.MemoryStorage;
+    export import FileStorage = JavascriptSDK.FileStorage;
+}
