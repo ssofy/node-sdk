ff449e854db3915b880f5dd7972222cd203d34b4
SSOfy <support@ssofy.com>
2023-08-26 13:50:04 +0300

    allow selecting additional columns

--- src/Transformers/UserTransformer.ts
+++ src/Transformers/UserTransformer.ts
@@ -3,43 +3,43 @@ import {Models} from '../Models'
 
 export class UserTransformer implements Transformers.Transformer {
     protected columns: { [key: string]: string };
+    protected additionalColumns: string[];
 
-    constructor(columnMap: { [key: string]: string }) {
+    constructor(columnMap: { [key: string]: string } = {}, additionalColumns: string[] = []) {
         this.columns = columnMap;
+        this.additionalColumns = additionalColumns;
     }
 
-    async transform(user: any): Promise<Models.UserEntity> {
-        let metadata = this.get(user, 'metadata', {});
+    async transform(data: any): Promise<Models.UserEntity> {
+        let metadata = this.get(data, 'metadata', {});
 
         if (typeof metadata === 'string') {
             metadata = JSON.parse(metadata) ?? {};
         }
 
-        const additional = Object.keys(user)
-            .filter(key => key !== this.columns.metadata)
-            .reduce((obj, key) => ({...obj, [key]: user[key]}), {});
-
         return {
             ...metadata,
-            id: this.get(user, 'id').toString(),
-            hash: this.get(user, 'hash').toString(),
-            name: this.get(user, 'name'),
-            display_name: this.get(user, 'display_name'),
-            picture: this.get(user, 'picture'),
-            username: this.get(user, 'username'),
-            email: this.get(user, 'email'),
-            email_verified: !!this.get(user, 'email_verified_at'),
-            phone: this.get(user, 'phone'),
-            phone_verified: !!this.get(user, 'phone_verified_at'),
-            additional
+            id: this.get(data, 'id', '').toString(),
+            hash: this.get(data, 'hash', '').toString(),
+            name: this.get(data, 'name'),
+            display_name: this.get(data, 'display_name'),
+            picture: this.get(data, 'picture'),
+            username: this.get(data, 'username'),
+            email: this.get(data, 'email'),
+            email_verified: !!this.get(data, 'email_verified_at'),
+            phone: this.get(data, 'phone'),
+            phone_verified: !!this.get(data, 'phone_verified_at'),
+            additional: this.additionalColumns.reduce((obj, key) => ({...obj, [key]: data[key]}), {}),
         };
     }
 
     private get(user: any, column: string, defaultVal: any = null): any {
         const mappedColumn = this.columns[column];
-        if (!mappedColumn || mappedColumn.trim() === '') {
+
+        if ((!mappedColumn || mappedColumn.trim() === '') && !user[column]) {
             return defaultVal;
         }
-        return user[mappedColumn] ?? defaultVal;
+
+        return user[mappedColumn ?? column] ?? defaultVal;
     }
 }
