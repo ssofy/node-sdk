daa6d62f6edbb55f50912095a1b64758b88217c8
SSOfy <support@ssofy.com>
2023-08-26 13:44:50 +0300

    fixed prisma schema issue

--- src/Datasource/PrismaConnection.ts
+++ src/Datasource/PrismaConnection.ts
@@ -11,26 +11,26 @@ export class PrismaConnection extends Connection {
     }
 
     async query(schema: any, criteria: Datasource.Criteria): Promise<Datasource.Item[]> {
-        return await this.client[schema].findMany({
+        return await schema.findMany({
             where: criteria,
         });
     }
 
     async insert(schema: any, row: Datasource.Criteria): Promise<Datasource.Item> {
-        return await this.client[schema].create({
+        return await schema.create({
             data: row,
         });
     }
 
     async update(schema: any, criteria: Datasource.Criteria, row: Datasource.Item): Promise<void> {
-        await this.client[schema].update({
+        await schema.update({
             where: criteria,
             data: row,
         });
     }
 
     async upsert(schema: any, criteria: Datasource.Criteria, row: Datasource.Item): Promise<void> {
-        await this.client[schema].upsert({
+        await schema.upsert({
             where: criteria,
             create: row,
             update: row,
@@ -38,7 +38,7 @@ export class PrismaConnection extends Connection {
     }
 
     async delete(schema: any, criteria: Datasource.Criteria): Promise<void> {
-        await this.client[schema].delete({
+        await schema.delete({
             where: criteria,
         });
     }
