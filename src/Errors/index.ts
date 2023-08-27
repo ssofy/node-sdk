779d877c022ded0b51c48bee3255e718706c91bf
SSOfy <support@ssofy.com>
2023-08-27 10:45:56 +0300

    exports update

--- src/Errors/index.ts
+++ src/Errors/index.ts
@@ -2,4 +2,3 @@ export {APIError} from "./APIError";
 export {InvalidTokenError} from "./InvalidTokenError";
 export {SignatureVerificationError} from "./SignatureVerificationError";
 export {TemplateNotFoundError} from "./TemplateNotFoundError";
-export {UnknownTemplateEngineError} from "./UnknownTemplateEngineError";
