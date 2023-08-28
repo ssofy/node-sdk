7fa3a1ac2b471c2352081ea8961d4fef34c7573d
SSOfy <support@ssofy.com>
2023-08-28 09:11:01 +0300

    renamed namespace to singular form

--- src/Storage/index.ts
+++ src/Storage/index.ts
@@ -1,6 +1,6 @@
 import * as JavascriptSDK from "@ssofy/javascript-sdk";
 
-export namespace Storages {
+export namespace Storage {
     export import Storage = JavascriptSDK.Storage;
     export import NullStorage = JavascriptSDK.NullStorage;
     export import MemoryStorage = JavascriptSDK.MemoryStorage;
